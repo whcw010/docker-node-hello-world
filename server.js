76b0c369eebf4a1cd427815e5da7fc4db3249dd5
whcw010 <whcw010@email.com>
2025-10-03 10:00:08 +0700

    change port back to 4000

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var http = require('http')
 
-var port = 4040
+var port = 4000
 
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {'Content-Type': 'text/plain'})
